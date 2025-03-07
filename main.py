73a98051c76490d6e070d9f125380ed9c65201c6
HARRY <hjohnson@student.saintaug.nsw.edu.au>
2025-03-07 12:01:59 +1100

    first print of data accesed from the csv file

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import csv
+questions = []
 with open("questions.csv", newline="") as questions:
         question_reader = csv.reader(questions)
+        for question in question_reader:
+            print(question)
 
 def main():
     pass
