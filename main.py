94935596c77f5afd7ad68c2ea40ae50ffee88635
HARRY <hjohnson@student.saintaug.nsw.edu.au>
2025-03-07 12:40:57 +1100

    loading questions from csv file function completed

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 import csv
-questions = []
-with open("questions.csv", newline="") as questions:
-        question_reader = csv.reader(questions)
-        for question in question_reader:
-            print(question)
+
+def load_questions():
+    questions = [] # list to store the questions
+    with open("questions.csv", newline="") as questions_file:
+            question_list = csv.reader(questions_file)
+            for question in question_list: # for the rows in the csv file
+                question_loading = {"topic": question[0], "question": question[1], "answer": question[2], "hint": question[3]} # create the loading question // item 0 in the row is the question, item 1 is the answer
+                questions.append(question_loading) # append the question to the list of questions
+
+    return questions
 
 def main():
-    pass
+    print(load_questions())
+
+main()
